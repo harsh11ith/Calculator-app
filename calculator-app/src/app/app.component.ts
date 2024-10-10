9088af6f00150f09a404375313badfc577c00fd0
harsh11ith <harshith11.edu@gmail.com>
2024-10-10 15:57:34 +0530

    Added all functionality

--- calculator-app/src/app/app.component.ts
+++ calculator-app/src/app/app.component.ts
@@ -1,13 +1,56 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { evaluate } from 'mathjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule], // Removed 'evaluate' from imports
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Fixed 'styleUrl' to 'styleUrls'
 })
 export class AppComponent {
   title = 'calculator-app';
+  toshow = '0';
+  currvalue = '';
+
+  writetoinput(value: string) {
+    this.currvalue = this.currvalue + value;
+    this.toshow = this.currvalue;
+  }
+
+  equals() {
+    try {
+      this.toshow = evaluate(this.currvalue); 
+      this.currvalue = this.toshow;
+    } catch (error) {
+      this.toshow = 'Error';
+    }
+  }
+
+  clear() {
+    this.currvalue = '';
+    this.toshow = '0';
+  }
+
+  // Slicing to remove the last element
+  back() {
+    this.currvalue = this.currvalue.slice(0, -1);
+    this.toshow = this.currvalue;
+    if (this.toshow == '') {
+      this.toshow = '0';
+    }
+  }
+
+  calevalue(solve: any) {
+    if (solve.charAt(0) == '0') {
+      solve = solve.slice(1);
+    }
+    try {
+      this.toshow = evaluate(solve); 
+    } catch (error) {
+      this.toshow = 'Error'; 
+    }
+  }
 }
